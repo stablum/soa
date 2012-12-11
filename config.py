2e153a2db09d65ce403b28d7d2f5b146333c1b0a
Francesco Stablum <stablum@gmail.com>
2012-12-11 11:17:59 +0100

    stuff

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 frac_edges = 0.1
 frac_damage 0.65
 factor_reuse = 0.25
-policy_id = 'simple
-
+policy_id = 'simple'
+init_num_nodes = len(g.nodes)
--- environment.py
+++ environment.py
@@ -4,14 +4,6 @@ import copy
 # local imports
 import config
 
-config = {
-    'frac_edges': 0.1,
-    'frac_damage': 0.65,
-    'factor_reuse': 0.25,
-    'policy_id': 'simple',
-	'init_num_nodes': len(g.nodes)
-}
-
 def environment():
     """ actions performed by the environment
         Alzeihmer desease:

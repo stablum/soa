a52a5634a0a3e5c24469fd8f51699861cadb9c60
milombo <m.colombo@students.uu.nl>
2012-12-20 18:41:16 +0100

    shortest path 2

--- shortest_path.py
+++ shortest_path.py
@@ -5,16 +5,16 @@ def shortest_path(start, end):
     if len(damaged) !=0:
         all_edges.discard(damaged.pop())
     dist, prev = {}, {}
-    sp_nodes=set([])
+    sp_nodes, sp_edges=set([]),set([])
     for v in g.nodes:
         dist[v], prev[v] = float('inf'), None
     dist[start] = 0
     for i in range(len(g.nodes)):   # Bellman-Ford algorithm, tweaked so that high weight = faster 
         for e in all_edges: # TODO: exclude from the count the original AB
-            if dist[e.target] > dist[e.source] + 1/e.weight:
-                dist[e.target] = dist[e.source] + 1/e.weight
+            if dist[e.target] > dist[e.source] + e.weight: #1/
+                dist[e.target] = dist[e.source] + e.weight
                 prev[e.target] = e
-                print (e.target, e.source)
+                print ( i, e.source, e.target, e.weight, '         ', e)
     for v in g.nodes:# color all the nodes and edges as RGB 200, 200, 200
         v.color = color(200, 200, 200)
     for e in g.edges:
@@ -22,12 +22,18 @@ def shortest_path(start, end):
     if dist[end] < float('inf'):    # highlight a shortest path with the green color (if one exists)
         e = prev[end]
         end.color = green
+        print e
         while e != None: #while
             e.color = green
             e.source.color = green
+            print (e.source, e.target, prev[e.source])
             e = prev[e.source]
-            sp_nodes.add(e)            #ee=prev[e.target] #mik     #sp_nodes.add(ee) #mik
-    return (dist[end], sp_nodes)   # return the total weight of the path (distance)
+            sp_edges.add(e)
+            sp_nodes.add(e.source)            #ee=prev[e.target] #mik     #sp_nodes.add(ee) #mik
+    return (dist[end], sp_nodes, sp_edges)   # return the total weight of the path (distance)
+
+shortest_path(v11, v4)
+
 
 if e.source == start e.target==start
     firstlaste.appen(e)

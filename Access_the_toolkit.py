b4ff82f57d3158ba694dc7b9cb24fb7c01347c3b
milombo <m.colombo@students.uu.nl>
2013-01-02 20:47:59 +0100

    access the toolkit + explanatorypowerpt + shrtestpath2 + stats

---
+++ Access_the_toolkit.py
@@ -0,0 +1,140 @@
+#-----IMPORT A DAMN CLASS FROM THE TOOLKIT 
+# the first step can be done in 2 ways:
+# the long way is to follow these instructions and get lost(<-- https://wiki.gephi.org/index.php/How_to_build_the_Gephi_Toolkit )
+# the short way is to exploit milombo's work and download the libs from the dropbox invitation 
+
+# So...
+# YOU SHOULD HAVE DOWNLOADED THE FILE FROM MY DROPBOX INVITATION IN YOUR MAIL>>> DO IT NOW!
+# DO THAT
+# NOW
+#
+# DONE?
+# OK, NOW ADJUST THE PATH TO ACCESS THE SUBFOLDER gephi-toolkit
+toolspath="C:\\Users\MangustaMegaMastar\\Downloads\\4gephi\\gephi-0.8.1-beta.sources\\gephi-0.8.1-beta.sources\\toolkit\\gephi-toolkit\\"
+
+algopath= toolspath+"org-gephi-algorithms.jar"
+graphpath= toolspath + "org-gephi-graph-api.jar"
+attr_path= toolspath + "org-gephi-data-attributes-api.jar"
+#attrtype_path=toolspath + " .jar"
+viz_path=toolspath + "org-gephi-visualization-api.jar"
+project_path=toolspath + "org-gephi-project-api.jar" 
+ioimporter_path=toolspath + "org-gephi-io-importer-api.jar"
+
+import java.io
+import sys
+sys.path.append(algopath)
+sys.path.append(graphpath)
+sys.path.append(algopath)
+sys.path.append(attr_path)
+sys.path.append(viz_path)
+sys.path.append(project_path)
+
+# Import single methods/interfaces as this:
+from org.gephi.graph.api import GraphView
+from org.gephi.algorithms.shortestpath import AbstractShortestPathAlgorithm
+from org.gephi.algorithms.shortestpath  import BellmanFordShortestPathAlgorithm
+
+#------------------------------------
+# this allowed to import methods/interfaces that were PREVIOUSLY UNACCESSIBLE.
+# WE NEED TO LEARN HOW TO USE THEM.
+# PROBABLY MORE OBJECTS ARE NEEDED.
+# IT IS NOW POSSIBLE TO DO THE SAME THAT THESE GUYS DO: http://wiki.gephi.org/index.php/Script_plugin
+# BELOW AN EXAMPLE FROM GEPHI SCRIPT-PLUGIN DEVELOPERS: NOT YET WORKING
+# GET INSPIRED BY IT AND MAKE IT WORKING (changing the import way as the 3 lines above)
+#-----------------------------------
+
+from org.gephi.graph.api import org.gephi.graph.api as graph_api
+import org.gephi.data.attributes.api as attributes_api
+import org.gephi.data.attributes.api.AttributeType as AttributeType
+import org.gephi.visualization.VizController as viz
+import org.gephi.project.api as project_api
+import org.gephi.io.importer.api as importer_api
+import java.io
+"""
+This file is loaded when the gephi script engine is used first (before the first user script execution)
+It contains the specific gephi python api accessible from script
+"""
+ 
+ 
+def help(obj = None) :
+    """
+    See help
+    """
+    if obj == None :
+        print "Python console"
+        print "Available functions : "
+        print dir(sys.modules[__name__])
+ 
+    else :
+        try :
+            print obj.__doc__
+        except :
+            print "Object has no documentation"
+ 
+def getGraphModel() :
+    """
+    getGraphModel() :
+    Return the graph model of the currently displayed graph.
+    """
+    return gephi.getLookup().lookup(graph_api.GraphController).getModel()
+ 
+def getAttributeModel() :
+    """
+    getAttributeModel() :
+    Return the attribute model of the currently displayed graph
+    """
+    return gephi.getLookup().lookup(attributes_api.AttributeController).getModel()
+ 
+ 
+#graph management
+def getDirectedGraph() :
+    """
+    getDirectedGraph() :
+    Get the currently displayed graph as a directed graph
+ 
+    Example :
+        dg = getDirectedGraph()
+        #values :
+        print "Nodes:", dg.getNodeCount(),"Edges: ", dg.getEdgeCount()
+ 
+        #iterating :
+        for e in dg.edges :
+            print "source : ", e.getSource(), "dest:", e.getTarget()
+        node2 = dg.getNode("n2") #using its id, see find_node to search with label.
+ 
+        print "Degree:", dg.getDegree(node2)
+    """
+    return getGraphModel().getDirectedGraph()
+ 
+def getGraph() :
+    """
+    getGraph() :
+        Get the currently displayed graph as a non directed graph
+ 
+    Example :
+    dg = getDirectedGraph()
+    #values :
+    print "Nodes:", dg.getNodeCount(),"Edges: ", dg.getEdgeCount()
+ 
+    #iterating :
+    for e in dg.edges :
+    print "source : ", e.getSource(), "dest:", e.getTarget()
+    node2 = dg.getNode("n2") #using its id, see find_node to search with label.
+ 
+    print "Degree:", dg.getDegree(node2)
+    """
+    return getGraphModel().getGraph()
+ 
+
+
+#----alternative untested method
+#In your case you probably want to use the path of your package to find the jar:
+# yourpackage/__init__.py
+import sys, os
+if 'java' in sys.platform.lower():
+    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                 "your-lib.jar"))
+    from jython_implementation import library
+else:
+    from cpython_implementation import library
+#Hope that helps!
---
+++ shortest_path2.py
@@ -0,0 +1,62 @@
+def shortest_path(start, end):
+    # initialize distances and predecessor edges
+    all_edges=g.edges # gi.edges()
+    damaged= start ? end
+    if len(damaged) !=0:
+        all_edges.discard(damaged.pop())
+    dist, prev = {}, {}
+    sp_nodes, sp_edges=set([]),set([])
+    for v in g.nodes:
+        dist[v], prev[v] = float('inf'), None
+    dist[start] = 0
+    for i in range(len(g.nodes)):   # Bellman-Ford algorithm, tweaked so that high weight = faster 
+        for e in all_edges: # TODO: exclude from the count the original AB
+            if dist[e.target] > dist[e.source] + e.weight: #1/
+                dist[e.target] = dist[e.source] + e.weight
+                prev[e.target] = e
+                print ( i, e.source, e.target, e.weight, '         ', e)
+    for e in all_edges:
+        if dist[e.target] > dist[e.source] + e.weight
+            error "Graph contains a negative-weight cycle"
+    for v in g.nodes:# color all the nodes and edges as RGB 200, 200, 200
+        v.color = color(200, 200, 200)
+    for e in g.edges:
+        e.color = color(200, 200, 200)
+    print (prev , dist)
+    if dist[end] < float('inf') or dist[end] = float('inf'):    # highlight a shortest path with the green color (if one exists)
+        e = prev[end]
+        end.color = green
+        print e
+        while e != None: #while
+            e.color = green
+            e.source.color = green
+            print (e.source, e.target, prev[e.source])
+            e = prev[e.source]
+    return (dist[end], prev, sp_nodes, sp_edges)   # return the total weight of the path (distance)
+
+shortest_path(v11, v4)
+
+
+    for v in g.nodes:# color all the nodes and edges as RGB 200, 200, 200
+        v.color = color(200, 200, 200)
+    for e in g.edges:
+        e.color = color(200, 200, 200)
+    if dist[end] < float('inf'):    # highlight a shortest path with the green color (if one exists)
+        e = prev[end]
+        end.color = green
+        print e
+        while e != None: #while
+            e.color = green
+            e.source.color = green
+            print (e.source, e.target, prev[e.source])
+            e = prev[e.source]
+            
+            
+if e.source == start e.target==start
+    firstlaste.appen(e)
+    
+    #\def get_equal(in_set, in_element):
+   for element in in_set:
+       if element == in_element:
+           return element
+   return None 

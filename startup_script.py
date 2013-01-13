a18a7a16c24880a0af2f9cfc1bacc5c2ba3f8410
Francesco Stablum <stablum@gmail.com>
2013-01-13 17:28:59 +0100

    changed something in the harmean and coding style

--- startup_script.py
+++ startup_script.py
@@ -32,17 +32,16 @@ def addpathjar(jarpath):
 
 def fixpaths():
     global jars # BE KIND TO OTHERS: do not cancel out their mypath, just comment it :)
-    mypath="D:\\ELECTIVES\\SELF_ORGA\\new_code\\" #mypath = "c:\\soa\\"
+    #mypath="D:\\ELECTIVES\\SELF_ORGA\\new_code\\" 
+    mypath = "c:\\soa\\"
     addpath(mypath)
     toolpath= mypath # for those who have the toolkit under the same folder
-    toolpath= "C:\\Users\\MangustaMegaMastar\\Downloads\\4gephi\\gephi-0.8.1-beta.sources\\gephi-0.8.1-beta.sources\\toolkit\\gephi-toolkit\\"
+    #toolpath= "C:\\Users\\MangustaMegaMastar\\Downloads\\4gephi\\gephi-0.8.1-beta.sources\\gephi-0.8.1-beta.sources\\toolkit\\gephi-toolkit\\"
     tk_path = toolpath+"gephi-toolkit\\"
     for jar in jars:
         addpathjar(tk_path+jar)
     addpathjar(toolpath+"gephi-toolkit.jar")
 
-fixpaths()
-    
 def startup():
     fixpaths()
     sys.modules.clear()

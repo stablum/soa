658444ed01c200831b946f249986a4c7cf8ac7e6
Francesco Stablum <stablum@gmail.com>
2013-01-14 21:11:37 +0100

    changed iteration counter, works the same and the code is prettier

--- stats.py
+++ stats.py
@@ -5,6 +5,7 @@ import gi
 import config
 import shorpath
 import main
+import os
 
 collector = None
 history = []
@@ -15,11 +16,8 @@ def new_collector_dict():
     """
     returns the dict that describes the current data collection "snapshot"
     """
-    global run_num
-    global count_iterations
-    count_iterations=main.iteration_counter()
     ret = {
-        'count_iterations':count_iterations,
+        'count_iterations':main.count_iterations,
         'num_kills':0,
         'total_weight':0,
         'path_length':0,    # http://gephi.org/docs/toolkit/org/gephi/statistics/plugin/GraphDistance.html#getPathLength()
@@ -54,7 +52,6 @@ def path_length():
     ret += shorpath.getpl()
     return ret
 
-
 def mean_edges_importance():
     ret = 0.0 # new
     ret += gi.get_mean_edges_importance()
@@ -65,8 +62,6 @@ def std_edges_importance():
     ret += gi.get_std_edges_importance()
     return ret 
 
-
-
 def snapshot():
     """ statistics and stuff (??) """
     global collector
@@ -75,6 +70,13 @@ def snapshot():
     collector.mean_edges_importance=mean_edges_importance()
     collector.std_edges_importance=std_edges_importance()
 
+def get_run_num_offset():
+    if os.path.exists(config.stats_filename):
+        ret = get_last_saved_run_num()
+    else:
+        ret = 0
+    return ret
+
 def write_history():
     """ write all statistics stored in memory on a csv file.
         *overwrites* previous file"""
@@ -82,12 +84,21 @@ def write_history():
     global run_num
     global count_iterations
     print "write_history; the run_num is now:"+str(run_num)+"\n"
+    
+    run_num_offset = get_run_num_offset()
     f = open(config.stats_filename, 'w')
     f.write("count_iterations,num_kills,total_weight,path_length,run_num,mean_edges_importance,std_edges_importance \n")
     f.flush()
     for c in history:
-        f.write(","+str(c.count_iterations)+","+str(c.num_kills)+","+str(c.total_weight)+","+str(c.path_length)+","+str(c.run_num)+","+str(c.mean_edges_importance)+","+str(c.std_edges_importance)+"\n")
-    f.write("\n")
-    f.flush()
+        f.write(str(c.count_iterations)+","+str(c.num_kills)+","+str(c.total_weight)+","+str(c.path_length)+","+str(run_num_offset+c.run_num)+","+str(c.mean_edges_importance)+","+str(c.std_edges_importance)+"\n")
+        f.flush()
     f.close()
+
+def get_last_saved_run_num():
+    """
+    returns the last saved run number
+    """
+    f = open(config.stats_filename, 'r')
+    last_line = f.readlines()[-1]
+    return int(last_line.split(",")[4])
 

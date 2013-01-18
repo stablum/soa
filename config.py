b9db6dc2e0b751295da376192e63c53a7b15c170
Francesco Stablum <stablum@gmail.com>
2013-01-18 23:08:31 +0100

    changed some stuff in conf

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ frac_damage = 0.65
 factor_reuse = 0.25
 policy_id = 'pro_poor'
 init_num_nodes = 0 # set by set_g
-max_iterations = 4
+max_iterations = 25
 treshold_kill = 0.2
 #mypath = "D:\\ELECTIVES\\SELF_ORGA\\new_code\\" # CHANGE ME!
 mypath = "C:\\soa\\" 
 stats_filename = mypath +"stats.csv" 
-runs_per_series = 3
+runs_per_series = 10
 
 _g = None
 gephi = None

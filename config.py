b611d7e10ec7f86f895516188eb7feea431c5590
milombo <m.colombo@students.uu.nl>
2013-01-19 11:46:40 +0100

    adding the icmb_weighted.. graph

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import utils
 frac_edges = 0.1
 frac_damage = 0.65
 factor_reuse = 0.25
-policy_id = 'passive'
+policy_id = 'simple'
 init_num_nodes = 0 # set by set_g
-max_iterations = 1
+max_iterations = 3
 treshold_kill = 0.2
 mypath = "D:\\ELECTIVES\\SELF_ORGA\\new_code\\" # CHANGE ME!
 #mypath = "C:\\soa\\" 
 #stats_filename = mypath +"stats_"+policy_id+".csv" 
 stats_filename = mypath +"stats.csv" 
-runs_per_series = 10
+runs_per_series = 3
 
 
 _g = None
